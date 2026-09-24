b0fba6f24600103e05530497cb1febf2385a9d08
Andrew Muratov <andrewamuratov@gmail.com>
2026-09-24 16:38:17 -0400

    feat: add Carleton campus identity to timetable model

--- src/lib/timetable-types.ts
+++ src/lib/timetable-types.ts
@@ -1,6 +1,6 @@
 export type ActivityType = "LEC" | "TUT" | "PRA" | "OTHER";
 export type Term = "Fall" | "Winter" | "Summer";
-export type Campus = "UTSG" | "UTM" | "UTSC" | "UNKNOWN";
+export type Campus = "UTSG" | "UTM" | "UTSC" | "CARLETON" | "UNKNOWN";
 export type Weekday =
   "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" | "Saturday" | "Sunday";
 export type MeetingLocationType = "physical" | "tba" | "online" | "unknown";
